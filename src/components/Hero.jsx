44a39a3a6dff612aa668a9c892a2952a5d2a16de
Shanjid Ahmed Rafi <shanjidahmed66@gmail.com>
2025-11-28 14:43:33 +0600

    Down indicator remove  in mobile and tab

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -96,7 +96,7 @@ const Hero = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 1.5 }}
-            className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
+            className="hidden md:block absolute bottom-8 left-1/2 transform -translate-x-1/2"
           >
             <motion.div
               animate={{ y: [0, 10, 0] }}

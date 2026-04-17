44d31fa969a58449a97e77697a2b63f17485184b
Shanjid Ahmed Rafi <shanjidahmed66@gmail.com>
2026-04-17 17:34:07 +0600

    Remove unused binary file wps_wid.cid-1138883127.1764595903.exe

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
@@ -40,4 +44,5 @@ export default {
     },
   },
   plugins: [],
-}
+};
+
